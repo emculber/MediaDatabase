f26f56f73d702d67477a7565c85b826fc7138dad
Erik <eculberson12@gmail.com>
2016-08-11 15:52:43 -0400

    Start of the user handlers

--- server_api/User_Handlers.go
+++ server_api/User_Handlers.go
@@ -5,6 +5,19 @@ import (
 	"net/http"
 )
 
+func startup(w http.ResponseWriter, r *http.Request) {
+	//TODO: Only allow this to run if there is no content available. gives full access
+	fmt.Fprintln(w, "startup Not Implimented")
+}
+
 func createUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "User Not Created Not Implimented")
+}
+
+func createRole(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Role Not Created Not Implimented")
+}
+
+func createPermission(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Permissions Not Created Not Implimented")
 }

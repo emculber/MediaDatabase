cdedb78a35df7f2fe6de5019c6540421999447a5
Erik <eculberson12@gmail.com>
2016-08-11 11:56:42 -0400

    Factor in limit

--- server_api/Finance_Service.go
+++ server_api/Finance_Service.go
@@ -6,12 +6,22 @@ func (transaction *Transaction) SplitMoney() {
 	wallets := transaction.UserKeys.getWalletList()
 	fmt.Println(wallets)
 
+	var wallet_diff float64
+	var unallocatedWallet Wallet
 	for _, wallet := range wallets {
 		fmt.Println("------")
 		fmt.Println("Transaction Amount", transaction.Amount)
 		fmt.Println("Wallet Percent", wallet.Percent)
 		if wallet.Percent <= 100 && wallet.Percent > 0 {
-			wallet.CurrentAmount += transaction.Amount * (wallet.Percent / 100)
+
+			wallet_amount := wallet.CurrentAmount + (transaction.Amount * (wallet.Percent / 100))
+			if wallet.Limit < (wallet_amount + wallet.CurrentAmount) {
+				wallet_diff += wallet_amount - wallet.Limit
+				wallet.CurrentAmount = wallet.Limit
+				wallet.Percent = 0
+			} else {
+				wallet.CurrentAmount = wallet_amount
+			}
 
 			fmt.Println("Wallet Decimal Percent", wallet.Percent/100)
 			fmt.Println("Wallet Amount", transaction.Amount*(wallet.Percent/100))
@@ -20,7 +30,15 @@ func (transaction *Transaction) SplitMoney() {
 				fmt.Println(err)
 			}
 			fmt.Println(wallet)
+			//TODO: Move this to its own sql statment
+			if wallet.Name == "unallocated" {
+				unallocatedWallet = wallet
+			}
 		}
+	}
+	unallocatedWallet.CurrentAmount += wallet_diff
+	if err := unallocatedWallet.updateWallet(); err != nil {
+		fmt.Println(err)
 	}
 	fmt.Println("------")
 }

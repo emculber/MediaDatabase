cd07bf91d231f3dd2864178a9ccf98a0b83adbfc
Erik <eculberson12@gmail.com>
2016-08-16 19:39:41 -0400

    Temp

--- server_api/Admin_Handlers.go
+++ server_api/Admin_Handlers.go
@@ -1,46 +1,45 @@
 package main
 
-import (
-	"net/http"
-
-	log "github.com/Sirupsen/logrus"
-)
+import "net/http"
 
 func createTables(w http.ResponseWriter, r *http.Request) {
 	//TODO: Register tables or functions to be create to spereate out dependencies
 	//TODO: Check for existing tables
 	//TODO: Only allow of superadmin/one person
 
-	userKeys := UserKeys{}
+	//TODO: UnBlock this when create user
+	/*
+		userKeys := UserKeys{}
 
-	r.ParseForm()
-	userKeys.Key = r.PostFormValue("key")
+		r.ParseForm()
+		userKeys.Key = r.PostFormValue("key")
 
-	if err := userKeys.validate(); err != nil {
+		if err := userKeys.validate(); err != nil {
+			log.WithFields(log.Fields{
+				"User Blame":      userKeys.User.Username,
+				"User Permission": userKeys.RolePermissions,
+				"User Key":        userKeys.Key,
+				"Error":           err,
+			}).Error("Error Validating User")
+			return
+		}
+
+		if err := userKeys.RolePermissions.checkAccess("execute"); err != nil {
+			log.WithFields(log.Fields{
+				"User Blame":      userKeys.User.Username,
+				"User Permission": userKeys.RolePermissions,
+				"User Key":        userKeys.Key,
+				"Error":           err,
+			}).Error("Error Checking Access")
+			return
+		}
+
 		log.WithFields(log.Fields{
 			"User Blame":      userKeys.User.Username,
 			"User Permission": userKeys.RolePermissions,
 			"User Key":        userKeys.Key,
-			"Error":           err,
-		}).Error("Error Validating User")
-		return
-	}
-
-	if err := userKeys.RolePermissions.checkAccess("execute"); err != nil {
-		log.WithFields(log.Fields{
-			"User Blame":      userKeys.User.Username,
-			"User Permission": userKeys.RolePermissions,
-			"User Key":        userKeys.Key,
-			"Error":           err,
-		}).Error("Error Checking Access")
-		return
-	}
-
-	log.WithFields(log.Fields{
-		"User Blame":      userKeys.User.Username,
-		"User Permission": userKeys.RolePermissions,
-		"User Key":        userKeys.Key,
-	}).Info("Running Create Tables")
+		}).Info("Running Create Tables")
+	*/
 	CreateUserTables()
 	CreateTables()
 	CreateFinanceTables()
